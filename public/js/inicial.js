7684465a7f057c314c33e99c4b9d53c55f6431fe
Tallyon <tallyon.lima@sptech.school>
2024-03-28 11:53:55 -0300

    Novo design da pagina index

---
+++ public/js/inicial.js
@@ -0,0 +1,44 @@
+// Menu Lateral 
+document.getElementById('menu-btn').addEventListener('change', function () {
+    var menu = document.querySelector('.itensNavMobile');
+    if (this.checked) {
+        menu.style.left = '0';
+    } else {
+        menu.style.left = '-1500px';
+    }
+});
+
+function fecharMenu() {
+    var menu = document.querySelector('.itensNavMobile');
+    var checkbox = document.getElementById('menu-btn');
+
+    if (this.checked) {
+        menu.style.left = '0';
+        checkbox.checked = true;
+    } else {
+        menu.style.left = '-1500px';
+        checkbox.checked = false;
+
+    }
+}
+
+
+//Rolagem suave 
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth'
+    });
+    this.checked = false;
+    fecharMenu()
+}
+
+
+function redirecionarLogin(){
+    window.location.href = "telaLogin.html";
+}
+
+function redirecionarCadastro(){
+    window.location.href = "cadastro.html";
+}
