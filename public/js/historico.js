0893be641ed808dd4452abbcb3a76cdd4dfe6577
ju-camargo <julia.cunha@sptech.school>
2024-04-08 20:44:16 -0300

    Alterações na tela de historico

---
+++ public/js/historico.js
@@ -0,0 +1,30 @@
+function abrirModal() {
+    document.getElementById('modal-relatorio').showModal();
+    document.getElementById('modal-relatorio').style.display = 'flex';
+  }
+  
+  function fecharModal() {
+    document.getElementById('modal-relatorio').close();
+    document.getElementById('modal-relatorio').style.display = 'none';
+  }
+  
+  function gerarPDF() {
+    var selectedDates = document.getElementById('data-relatorio').value;
+    var datesArray = selectedDates.split(', ');
+  
+    var doc = new jsPDF();
+    doc.text('Relatório dos dias selecionados:', 10, 10);
+    datesArray.forEach(function(date, index) {
+      doc.text(date, 10, 20 + (index * 10));
+    });
+    doc.save('relatorio.pdf');
+  
+    fecharModal();
+  }
+  
+  // Inicializa o Flatpickr
+  flatpickr("#data-relatorio", {
+    // mode: "multiple",
+    mode: "range",
+    dateFormat: "Y-m-d",
+  });
