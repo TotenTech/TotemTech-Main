7329693a8569d5d8d06306e0990722f41b66b268
ju-camargo <julia.cunha@sptech.school>
2024-04-15 21:31:06 -0300

    Alterações no botão de gerar relatório da tela de histórico

---
+++ login-totemtech-client/src/main/java/model/User.java
@@ -0,0 +1,12 @@
+package model;
+
+public class User {
+
+        public String email;
+        String senha;
+
+        public User(String email, String senha) {
+            this.email = email;
+            this.senha = senha;
+        }
+}
--- public/js/dashboard/historico.js
+++ public/js/dashboard/historico.js
@@ -1,36 +1,58 @@
 
 window.addEventListener("DOMContentLoaded", () => {
-    if (sessionStorage.ID_USUARIO == undefined) {
-        window.location.href = "/erro";
-    }
+  if (sessionStorage.ID_USUARIO == undefined) {
+    window.location.href = "/erro";
+  }
 })
 
+//"seleciona uma data"
+function filtrarDados() {
+  var filtroTotens = document.getElementById("selecao-filtro").value;
+  var filtroData = document.getElementById("data").value;
+
+  var linhas = document.getElementById("corpo_tabela").getElementsByTagName("tr");
+  for (var i = 0; i < linhas.length; i++) {
+    var linha = linhas[i];
+    var totem = linha.cells[0].innerText;
+    var data = linha.cells[1].innerText.split(' ')[0]; 
+
+    var correspondeTotens = (filtroTotens === "totens" || totem === filtroTotens);
+    var correspondeData = (!filtroData || data === filtroData);
+
+    if (correspondeTotens && correspondeData) {
+      linha.style.display = ""; 
+    } else {
+      linha.style.display = "none"; 
+    }
+  }
+}
+
 function abrirModal() {
-    // document.getElementById('modal-relatorio').showModal();
-    document.getElementById('modal-relatorio').style.display = 'flex';
-  }
-  
-  function fecharModal() {
-    // document.getElementById('modal-relatorio').close();
-    document.getElementById('modal-relatorio').style.display = 'none';
-  }
-  
-  function gerarPDF() {
-    var selectedDates = document.getElementById('data-relatorio').value;
-    var datesArray = selectedDates.split(', ');
-  
-    var doc = new jsPDF();
-    doc.text('Relatório dos dias selecionados:', 10, 10);
-    datesArray.forEach(function(date, index) {
-      doc.text(date, 10, 20 + (index * 10));
-    });
-    doc.save('relatorio.pdf');
-  
-    fecharModal();
-  }
+  // document.getElementById('modal-relatorio').showModal();
+  document.getElementById('modal-relatorio').style.display = 'flex';
+}
+
+function fecharModal() {
+  // document.getElementById('modal-relatorio').close();
+  document.getElementById('modal-relatorio').style.display = 'none';
+}
+
+function gerarPDF() {
+  var selectedDates = document.getElementById('data-relatorio').value;
+  var datesArray = selectedDates.split(', ');
+
+  var doc = new jsPDF();
+  doc.text('Relatório dos dias selecionados:', 10, 10);
+  datesArray.forEach(function (date, index) {
+    doc.text(date, 10, 20 + (index * 10));
+  });
+  doc.save('relatorio.pdf');
+
+  fecharModal();
+}
 
 
 document.getElementById("btn_sair").addEventListener("click", () => {
-    sessionStorage.clear();
-    window.location.href = "/";
+  sessionStorage.clear();
+  window.location.href = "/";
 })
