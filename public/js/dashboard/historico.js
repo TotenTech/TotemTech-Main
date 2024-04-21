325b3d3da965c02e5aff6655a554e3d14fbbbb07
ju-camargo <julia.cunha@sptech.school>
2024-04-21 17:52:10 -0300

    Arrumando a lógica para filtrar totens na tela de histórico

--- public/js/dashboard/historico.js
+++ public/js/dashboard/historico.js
@@ -5,24 +5,47 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 })
 
+// select totens
+function filtrarTotens() {
+  // Obtém o valor selecionado no menu de filtro
+  var filtro = document.getElementById("selecao-filtro").value;
+
+  // Obtém todas as linhas da tabela
+  var linhas = document.getElementById("corpo_tabela").getElementsByTagName("tr");
+
+  // Percorre todas as linhas da tabela
+  for (var i = 0; i < linhas.length; i++) {
+      var linha = linhas[i];
+      var idTotem = linha.getAttribute("id");
+
+      // Se o filtro for "todos" ou se o ID do totem corresponder ao filtro selecionado
+      if (filtro === "totens" || filtro === idTotem) {
+          // Exibe a linha
+          linha.style.display = "";
+      } else {
+          // Oculta a linha
+          linha.style.display = "none";
+      }
+  }
+}
+
 //"seleciona uma data"
 function filtrarDados() {
-  var filtroTotens = document.getElementById("selecao-filtro").value;
+  // var filtroTotens = document.getElementById("selecao-filtro").value;
   var filtroData = document.getElementById("data").value;
 
   var linhas = document.getElementById("corpo_tabela").getElementsByTagName("tr");
   for (var i = 0; i < linhas.length; i++) {
     var linha = linhas[i];
-    var totem = linha.cells[0].innerText;
-    var data = linha.cells[1].innerText.split(' ')[0]; 
+    var data = linha.cells[1].innerText.split(' ')[0];
 
-    var correspondeTotens = (filtroTotens === "totens" || totem === filtroTotens);
+    //var correspondeTotens = (filtroTotens === "totens" || totem === filtroTotens);
     var correspondeData = (!filtroData || data === filtroData);
 
-    if (correspondeTotens && correspondeData) {
-      linha.style.display = ""; 
+    if (correspondeData) {
+      linha.style.display = "";
     } else {
-      linha.style.display = "none"; 
+      linha.style.display = "none";
     }
   }
 }
