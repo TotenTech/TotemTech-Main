e9fba13791e9660342f4d9bb7a5cca3be11957cc
Tallyon <tallyon.lima@sptech.school>
2024-04-27 16:25:59 -0300

    Novo layout de todas as paginas da dashboard

--- public/js/dashboard/estatisticas.js
+++ public/js/dashboard/estatisticas.js
@@ -18,82 +18,62 @@ const dadosRAM = [60, 65, 70, 75, 80, 85, 90]; // uso de RAM (%)
 const dadosDISCO = [5, 6, 7, 6, 5, 4, 3]; // tempo médio de resposta do disco (ms)
 const dadosDownload = [20, 25, 30, 35, 30, 25, 20]; // utilização da rede - Download (%)
 const dadosUpload = [10, 15, 20, 15, 10, 15, 20]; // utilização da rede - Upload (%)
-const labels = ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
+const labelSem = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'];
 
-// tentativa de converter os horários para minutos
-const minutos = labels.map(label => {
-    const [hora, minuto] = label.split(':');
-    return (parseInt(hora) * 60) + parseInt(minuto);
-});
+const data = {
+    labels: labelSem,
+    datasets: [{
+        label: 'Todas as interrupções',
+        data: [2, 4, 6, 3, 8, 5, 7], // Valores menores que 10
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+    }]
+};
 
-const ctx1 = document.getElementById('graficoLinhas');
+const ctx1 = document.getElementById('graficoBar');
 
 const chart1 = new Chart(ctx1, {
-    type: 'line',
-    data: {
-        labels: minutos,
-        datasets: [{
-            label: 'CPU',
-            data: dadosCPU,
-            borderColor: '#D6618F',
-            fill: false
-        }, {
-            label: 'RAM',
-            data: dadosRAM,
-            borderColor: '#F1931B',
-            fill: false
-        }, {
-            label: 'Disco',
-            data: dadosDISCO,
-            borderColor: '#888C46',
-            fill: false
-        }, {
-            label: 'Download',
-            data: dadosDownload,
-            borderColor: '#3E6BA8',
-            fill: false
-        }, {
-            label: 'Upload',
-            data: dadosUpload,
-            borderColor: '#BD4E46',
-            fill: false
-        }]
-    },
+    type: 'bar',
+    data: data,
     options: {
         scales: {
-            x: {
-                type: 'linear',
-                ticks: {
-                    callback: function (value, index, values) {
-                        const horas = Math.floor(value / 60);
-                        const minutos = value % 60;
-                        return `${horas}:${minutos < 10 ? '0' : ''}${minutos}`;
-                    }
-                }
-            },
             y: {
                 beginAtZero: true
-            }
-        },
-        plugins: {
-            legend: {
-                position: 'right'
             }
         }
     }
 });
 
 
+
+
 //GRAFICO PIZZA : uso de recursos
 // dados mockados
-const dadosDistribuicao = [30, 20, 25, 25]; // percentuais CPU, RAM, disco, rede
+const dadosDistribuicao = [12, 4, 2, 1]; // percentuais CPU, RAM, disco, rede
 const labelsDistribuicao = ['CPU', 'RAM', 'Disco', 'Rede'];
 const cores = ['#D6618F', '#F1931B', '#888C46', '#3E6BA8'];
 
 const ctx2 = document.getElementById('graficoPizza');
 
 const chart2 = new Chart(ctx2, {
-    type: 'pie',
+    type: 'doughnut',
     data: {
         labels: labelsDistribuicao,
         datasets: [{
--- public/js/dashboard/totens.js
+++ public/js/dashboard/totens.js
@@ -2,132 +2,172 @@ window.addEventListener("DOMContentLoaded", () => {
     if (sessionStorage.ID_USUARIO == undefined) {
         window.location.href = "/erro";
     }
-})
- 
- //legenda cores - status totens
- document.getElementById('circle-legenda').addEventListener('click', function () {
-    var helpLegenda = document.getElementById('legenda-help');
-    if (helpLegenda.style.display === 'none') {
-        helpLegenda.style.display = 'block';
-    } else {
-        helpLegenda.style.display = 'none';
-    }
 });
+
+// Div que totem todos os graficos e linhas
+const boxGrafico = document.getElementById('divBoxGrafico');
+
+//Linha da descrição do gráfico
+const lineDescricao = document.getElementById('boxLineDescricao');
+
+// Local de plotar o gráfico
+const localChart = document.getElementById('boxLocalChart');
+
 
 
 //gráfico:
 // Dados de exemplo 
-const dadosCPU = [40, 50, 60, 70, 65, 55, 45]; // Utilização da CPU (%)
-const dadosRAM = [60, 65, 70, 75, 80, 85, 90]; // Uso de RAM (%)
-const dadosDISCO = [5, 6, 7, 6, 5, 4, 3]; // Tempo médio de resposta do disco (ms)
-const dadosRede = [20, 25, 30, 35, 30, 25, 20]; // Utilização da rede - Download (%)
-const dadosUpload = [10, 15, 20, 15, 10, 15, 20]; // Utilização da rede - Upload (%)
-const labels = ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'];
+const dadosCPU = [  48, 49, 50, 56, 57, 58, 59, 60, 51, 52, 53, 42, 43, 44, 45,40, 41,54, 55, 46, 47]; // Utilização da CPU (%)
+const dadosRAM = [60, 65, 68, 70, 63, 67, 69, 64, 62, 90, 66, 61, 91, 71, 89, 88, 87, 86, 85, 84, 83]; // Uso de RAM (%)
+const dadosDISCO = [5, 6, 7, 6, 5, 4, 3, 3, 4, 6, 5, 7, 3, 4, 6, 5, 7]; // Tempo médio de resposta do disco (ms)
+const dadosRede = [20, 25, 30, 35, 30, 25, 20, 15, 20, 15, 10, 15, 20, 15, 20, 15, 10, 15, 20]; // Utilização da rede - Download (%)
 
-const ctxCpu = document.getElementById('graficoCPU');
-const ctxRam = document.getElementById('graficoRAM');
-const ctxDisco = document.getElementById('graficoDisco');
-const ctxRede = document.getElementById('graficoRede');
 
-const chart = new Chart(ctxCpu, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: 'CPU',
-            data: dadosCPU,
-            borderColor: '#D6618F',
-            fill: false
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+const labels = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00'];
+
+
+abrirGrafico();
+function abrirGrafico(tipo) {
+
+    if (tipo == "rede") {
+        boxGrafico.style.backgroundColor = "rgba(131, 74, 161, 0.8)";
+        lineDescricao.innerHTML = `Monitoramento da Rede nas últimas 24 horas em %`;
+        localChart.innerHTML = `<canvas class="graficoRede" id="graficoRede"></canvas>`;
+        const ctxRede = document.getElementById('graficoRede');
+
+
+        const chartQuatro = new Chart(ctxRede, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Rede',
+                    data: dadosRede,
+                    borderColor: '#FFFFFF',
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'right'
+                    }
+                }
             }
-        },
-        plugins: {
-            legend: {
-                position: 'right'
+        });
+
+    } else if (tipo == "ram") {
+        boxGrafico.style.backgroundColor = "rgba(245, 199, 126, 1)";
+        lineDescricao.innerHTML = `Monitoramento da memória RAM nas últimas 24 horas em %`;
+        localChart.innerHTML = `<canvas class="graficoRAM" id="graficoRAM"></canvas>`;
+        const ctxRam = document.getElementById('graficoRAM');
+
+        const chartDois = new Chart(ctxRam, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'RAM',
+                    data: dadosRAM,
+                    borderColor: '#FFFFFF',
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'right'
+                    }
+                }
             }
-        }
+        });
+
+    } else if (tipo == "disco") {
+        boxGrafico.style.backgroundColor = "rgba(117, 199, 126, 1)";
+        lineDescricao.innerHTML = `Monitoramento do Disco nas últimas 24 horas em %`;
+        localChart.innerHTML = `<canvas class="graficoDisco" id="graficoDisco"></canvas>`;
+        const ctxDisco = document.getElementById('graficoDisco');
+
+
+        const chartTres = new Chart(ctxDisco, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Disco',
+                    data: dadosDISCO,
+                    borderColor: '#FFFFFF',
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'right'
+                    }
+                }
+            }
+        });
+    } else {
+        boxGrafico.style.backgroundColor = "rgba(136, 187, 204, 0.8)";
+        lineDescricao.innerHTML = ` Monitoramento da Cpu nas últimas 24 horas em %`;
+        localChart.innerHTML = `<canvas class="graficoCPU" id="graficoCPU"></canvas>`;
+        const ctxCpu = document.getElementById('graficoCPU');
+
+        const chart = new Chart(ctxCpu, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'CPU',
+                    data: dadosCPU,
+                    borderColor: '#FFFFFF',
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                plugins: {
+                    legend: {
+                        position: 'right'
+                    }
+                }
+            }
+        });
+
     }
-});
+}
 
-const chartDois = new Chart(ctxRam, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: 'RAM',
-            data: dadosRAM,
-            borderColor: '#F1931B',
-            fill: false
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        },
-        plugins: {
-            legend: {
-                position: 'right'
-            }
-        }
-    }
-});
 
-const chartTres = new Chart(ctxDisco, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: 'Disco',
-            data: dadosDISCO,
-            borderColor: '#888C46',
-            fill: false
-    }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        },
-        plugins: {
-            legend: {
-                position: 'right'
-            }
-        }
-    }
-});
+//  //legenda cores - status totens
+//  document.getElementById('circle-legenda').addEventListener('click', function () {
+//     var helpLegenda = document.getElementById('legenda-help');
+//     if (helpLegenda.style.display === 'none') {
+//         helpLegenda.style.display = 'block';
+//     } else {
+//         helpLegenda.style.display = 'none';
+//     }
+// });
 
-const chartQuatro = new Chart(ctxRede, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: 'Rede',
-            data: dadosRede,
-            borderColor: '#3E6BA8',
-            fill: false
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        },
-        plugins: {
-            legend: {
-                position: 'right'
-            }
-        }
-    }
-});
 
 document.getElementById("btn_sair").addEventListener("click", () => {
     sessionStorage.clear();
