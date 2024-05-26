1a18a5e82580028ad4472eb00e4c686999008c09
Marayah <marayah.pintos@sptech.school>
2024-05-25 23:48:19 -0300

    Subi a nova tela de estatística da dashboard

--- public/js/dashboard/estatisticas.js
+++ public/js/dashboard/estatisticas.js
@@ -18,21 +18,20 @@ const dadosRAM = [60, 65, 70, 75, 80, 85, 90]; // uso de RAM (%)
 const dadosDISCO = [5, 6, 7, 6, 5, 4, 3]; // tempo médio de resposta do disco (ms)
 const dadosDownload = [20, 25, 30, 35, 30, 25, 20]; // utilização da rede - Download (%)
 const dadosUpload = [10, 15, 20, 15, 10, 15, 20]; // utilização da rede - Upload (%)
-const labelSem = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado'];
+const labelSem = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00','13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00','23:00'];
 
 const data = {
     labels: labelSem,
     datasets: [{
         label: 'Todas as interrupções',
-        data: [2, 4, 6, 3, 8, 5, 7], // Valores menores que 10
+        data: [2, 4, 6, 3, 8, 5, 7, 2, 3, 5, 7, 3, 6, 1, 3, 2, 4, 2, 9, 10, 6, 2, 3, 1], // Valores menores que 10
         backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(255, 159, 64, 0.2)',
             'rgba(255, 205, 86, 0.2)',
             'rgba(75, 192, 192, 0.2)',
             'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
+            
         ],
         borderColor: [
             'rgb(255, 99, 132)',
@@ -50,7 +49,7 @@ const data = {
 const ctx1 = document.getElementById('graficoBar');
 
 const chart1 = new Chart(ctx1, {
-    type: 'bar',
+    type: 'line',
     data: data,
     options: {
         scales: {
@@ -68,7 +67,7 @@ const chart1 = new Chart(ctx1, {
 // dados mockados
 const dadosDistribuicao = [12, 4, 2, 1]; // percentuais CPU, RAM, disco, rede
 const labelsDistribuicao = ['CPU', 'RAM', 'Disco', 'Rede'];
-const cores = ['#D6618F', '#F1931B', '#888C46', '#3E6BA8'];
+const cores = ['#0000CD', '	#6495ED', '#1e90ff', '#3E6BA8'];
 
 const ctx2 = document.getElementById('graficoPizza');
 
